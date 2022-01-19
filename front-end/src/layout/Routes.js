af7825c6466515a9ce26d877fdff0732e3f037a3
Tom Officer <thomasofficerjr@gmail.com>
2022-01-18 20:56:34 -0800

    initial repo change

---
+++ front-end/src/layout/Routes.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
+import Dashboard from "../dashboard/Dashboard";
+import NotFound from "./NotFound";
+import NewReservation from "./NewReservation";
+import NewTable from "./NewTable";
+import SeatReservation from "./SeatReservation";
+import SearchPhone from "./SearchPhone";
+import EditReservation from "./EditReservation";
+
+function Routes() {
+  return (
+    <Switch>
+      <Route exact={true} path="/">
+        <Redirect to={"/dashboard"} />
+      </Route>
+      <Route exact path="/reservations">
+        <Redirect to={"/dashboard"} />
+      </Route>
+      <Route exact path="/reservations/new">
+        <NewReservation />
+      </Route>
+      <Route path="/reservations/:reservation_id/seat">
+        <SeatReservation />
+      </Route>
+      <Route path="/reservations/:reservation_id/edit">
+        <EditReservation />
+      </Route>
+      <Route path="/tables/new">
+        <NewTable />
+      </Route>
+      <Route path="/dashboard">
+        <Dashboard />
+      </Route>
+      <Route path="/dashboard/:date">
+        <Dashboard />
+      </Route>
+      <Route path="/search">
+        <SearchPhone />
+      </Route>
+      <Route>
+        <NotFound />
+      </Route>
+    </Switch>
+  );
+}
+
+export default Routes;
